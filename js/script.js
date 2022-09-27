81ab1707d12c7432282148c695a21de2f529c875
astra9 <thedigitalwhiterabbit@gmail.com>
2022-09-27 05:16:52 -0700

    finished-project
    completed project. Added font-awesome to the project and added the about
    page dynamically.
    
    Signed-off-by: astra9 <thedigitalwhiterabbit@gmail.com>

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ var menuItemsUrl =
   "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
 var menuItemsTitleHtml = "snippets/menu-items-title.html";
 var menuItemHtml = "snippets/menu-item.html";
+var aboutHtmlUrl = "snippets/about.html"; //ABOUT PAGE
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
@@ -83,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -101,8 +102,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
-
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -114,15 +114,14 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      //
-      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage =  insertProperty(homeHtml,"randomCategoryShortName", `'${chosenCategoryShortName}'`)
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -155,6 +154,31 @@ dc.loadMenuItems = function (categoryShort) {
     menuItemsUrl + categoryShort,
     buildAndShowMenuItemsHTML);
 };
+
+//LOADS THE ABOUT PAGE
+dc.loadAboutPage = function (){
+  $ajaxUtils.sendGetRequest(
+    aboutHtmlUrl,
+    function (aboutHtml) {
+      let newAboutHtml = aboutHtml
+      let randomNum = Math.floor(Math.random()*5)+1;
+      for(let i=1; i<=5; i++){
+          let toSubstitute = "toSubstitute-"+i;
+          console.log(toSubstitute)
+          if(i<=randomNum){
+            newAboutHtml =  insertProperty(newAboutHtml,toSubstitute, 'fa-solid fa-star')
+          }
+          else{
+            newAboutHtml =  insertProperty(newAboutHtml,toSubstitute, 'fa-regular fa-star')
+          }
+          console.log(newAboutHtml)
+      }
+      toSubstitute="toSubstitute"
+      newAboutHtml =  insertProperty(newAboutHtml, toSubstitute, `${randomNum}-star rating`)
+      insertHtml("#main-content", newAboutHtml);
+    },
+    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+}
 
 
 // Builds HTML for the categories page based on the data
